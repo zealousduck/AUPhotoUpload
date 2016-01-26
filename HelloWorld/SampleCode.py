2b712954e1851facb9d0df6c3bc648fde015c381
auphotoupload <auphotoupload@gmail.com>
2016-01-26 08:29:18 -0600

    Demonstration code for Dr Qin; initial commit from lab machine

---
+++ HelloWorld/SampleCode.py
@@ -0,0 +1,18 @@
+'''
+Created on Jan 22, 2016
+
+@author: photos
+'''
+
+class SampleCode(object):
+
+    def __init__(self):
+        pass
+    
+    def HelloWorld(self):
+        print("Hello, World!")
+        
+        
+        
+foo = SampleCode()
+foo.HelloWorld()
