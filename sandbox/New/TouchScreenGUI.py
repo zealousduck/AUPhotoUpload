93fd3d5e05be47086693fd764f5aa63ce68e8236
zealousduck <stewartpatrick12@gmail.com>
2016-02-23 08:04:58 -0600

    Object-oriented version of Stacy's GUI code

--- sandbox/New/TouchScreenGUI.py
+++ sandbox/New/TouchScreenGUI.py
@@ -7,63 +7,62 @@ from Tkinter import *
 from production.Supervisor import Supervisor
 from multiprocessing import Process
 
-#create a title for the window
-#running = False
-
-root = Tk()
-root.wm_title("AU Photo Upload")
-
-#place an image in the window
-img = PhotoImage(file='tiger.gif')
-root.tk.call('wm', 'iconphoto', root._w, img)
-
-#functions defined to run scripts
-
-def UploadPhotos(event):
-    print("Test for script to upload photos")
+class FrontEnd(object):
     
-    running = False
+    def __init__(self):
+        self.toggle = False     # Variable for constant-upload mode 
+        self.root = self.TkSetup() 
     
-    if running is False:
-        supervisor = Supervisor()
-        process = Process(target=supervisor.run())
-        running = True
-        process.start()
-        process.join()
+    def run(self):
+        self.root.mainloop()
+    
+    def TkSetup(self):
+        root = Tk()
+        root.wm_title("AU Photo Upload")
+        #img = PhotoImage(file='tiger.gif')
+        #root.tk.call('wm', 'iconphoto', root._w, img)
+        label = Label(root, text="AU Photo Upload", bg="orange", fg="white", font = "Verdana 15")
+        label.pack(fill=X)
         
-    if running is True:
-        process.terminate()  
-        running = False
+        topFrame = Frame(root)
+        topFrame.pack()
+        
+        #information for Upload Photos (continuous)
+        button1 = Button(topFrame, text="Upload Photos", width=12, height=12, bg="orange", fg="white", font = "Verdana 12")
+        button1.bind("<Button-1>", self.ContinuousUploadToggle)
+        
+        #information for button2
+        button2 = Button(topFrame, text="File Explorer", width=12, height=12, bg="orange", fg="white", font = "Verdana 12")
+        button2.bind("<Button-2>", self.FileExplorer)
+        
+        #information for button3
+        button3 = Button(topFrame, text="Settings", width=12, height=12, bg="orange", fg="white", font = "Verdana 12")
+        button3.bind("<Button-3>", self.Settings)
+        
+        #pack all information for the buttons 
+        button1.pack(side=LEFT)
+        button2.pack(side=LEFT)
+        button3.pack(side=LEFT)
+        return root
 
-def FileExplorer(event):
-    print("Test for script to file explorer")
-
-def Settings(event):
-    print("Test for script to settings")
+    def ContinuousUploadToggle(self, event):
+        print("Test for script to upload photos")       
+        if self.toggle is False:
+            supervisor = Supervisor()
+            process = Process(target=supervisor.run())
+            self.toggle = True
+            process.start()
+            # process.join()     
+        if self.toggle is True:
+            process.terminate()  
+            self.toggle = False
+    
+    def FileExplorer(self, event):
+        print("Test for script to file explorer")
+    
+    def Settings(self, event):
+        print("Test for script to settings")
+        
+if __name__ == '__main__':
+    FrontEnd().run()
   
-#header label at the top of window  
-label = Label(root, text="AU Photo Upload", bg="orange", fg="white", font = "Verdana 15")
-label.pack(fill=X)
-
-topFrame = Frame(root)
-topFrame.pack()
-
-#information for button1
-button1 = Button(topFrame, text="Upload Photos", width=12, height=12, bg="orange", fg="white", font = "Verdana 12")
-button1.bind("<Button-1>", UploadPhotos)
-
-#information for button2
-button2 = Button(topFrame, text="File Explorer", width=12, height=12, bg="orange", fg="white", font = "Verdana 12")
-button2.bind("<Button-2>", FileExplorer)
-
-#information for button3
-button3 = Button(topFrame, text="Settings", width=12, height=12, bg="orange", fg="white", font = "Verdana 12")
-button3.bind("<Button-3>", Settings)
-
-#pack all information for the buttons 
-button1.pack(side=LEFT)
-button2.pack(side=LEFT)
-button3.pack(side=LEFT)
-
-
-root.mainloop()
