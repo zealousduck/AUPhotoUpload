aadb90aef0963554bbacc38cf675abdaefd000e8
emseduau <ems0025@auburn.edu>
2016-02-26 23:31:16 -0600

    Update TouchScreenGUI.py

--- production/TouchScreenGUI.py
+++ production/TouchScreenGUI.py
@@ -31,38 +31,34 @@ class FrontEnd(object):
         topFrame.pack()
         
         #information for Upload Photos (continuous)
-        button1 = Button(topFrame, text="Upload Photos", width=12, height=12, bg="orange", fg="white", font = "Verdana 12")
-        button1.bind("<Button-1>", self.ContinuousUploadToggle)
+        self.button1 = Button(topFrame, text="Upload Photos: \nOFF", width=14, height=12, bg="orange", fg="white", font = "Verdana 12")
+        self.button1.bind("<Button-1>", self.ContinuousUploadToggle)
         
         #information for button2
-        button2 = Button(topFrame, text="File Explorer", width=12, height=12, bg="orange", fg="white", font = "Verdana 12")
-        button2.bind("<Button-1>", self.FileExplorer)
+        self.button2 = Button(topFrame, text="File Explorer", width=14, height=12, bg="orange", fg="white", font = "Verdana 12")
+        self.button2.bind("<Button-1>", self.FileExplorer)
         
         #information for button3
-        button3 = Button(topFrame, text="Settings", width=12, height=12, bg="orange", fg="white", font = "Verdana 12")
-        button3.bind("<Button-1>", self.Settings)
+        self.button3 = Button(topFrame, text="Settings", width=14, height=12, bg="orange", fg="white", font = "Verdana 12")
+        self.button3.bind("<Button-1>", self.Settings)
         
         #pack all information for the buttons 
-        button1.pack(side=LEFT)
-        button2.pack(side=LEFT)
-        button3.pack(side=LEFT)
+        self.button1.pack(side=LEFT)
+        self.button2.pack(side=LEFT)
+        self.button3.pack(side=LEFT)
         return root
 
     def ContinuousUploadToggle(self, event):
-         
         if self.toggle is False:
             print("Turning on photo upload...")    
-            #process = Process(target= self.startSupervisor)
-            #process.start()
             self.toggle = True
             self.queue.put("ContinuousUploadCreate")
-            #self.root = self.TkSetup()
-            #self.run()
-            # process.join()     
+            self.button1.config(text="Upload Photos: \nON")
         else: 
             print("Turning off photo upload...")
             self.toggle = False
             self.queue.put("ContinuousUploadKill")
+            self.button1.config(text="Upload Photos: \nOFF")
     
     def FileExplorer(self, event):
         print("Test for script to file explorer")
