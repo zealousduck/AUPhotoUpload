593c084fd87015a4da6621a211ad92e994b80559
bootlessxfly <bootlessxfly@gmail.com>
2016-02-28 17:47:42 -0600

    Camera inital commit. Has wait-event and capute working with error handling

---
+++ production/CamReader.py
@@ -0,0 +1,41 @@
+'''
+Created on Feb 28, 2016
+
+@author: chris
+'''
+import subprocess as sb
+import os
+from threading import Timer
+
+class CamReader(object):
+    '''
+    classdocs
+    '''
+    shell = True;
+    curPID = None;
+    dirPhoto = "~/Pictures/Dropbox"
+    dirThumb = "~/Pictures/Thumbnail"
+    dirBrowser = "~/Picture/Browser"
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+    
+    def wait_event_download(self):
+        try:
+            # Adds pictures to the stored photo directory. If no camera is supported, we raise an error.
+            sb.check_output(["gphoto2", "--wait-event-and-download", "--skip-existing", self.dirBrowser], self.shell);
+        except sb.CalledProcessError:
+            print "Camera is either not connected or not supported"
+            # Here we need to figure out proper error handling
+            self.wait_event_download(); #Continues to attempt the connection in till killed
+                
+            
+        self.curPID = os.getpid(); #Use this pid to toggle off. Using kill.
+        #print self.curPID;
+    
+#main test
+read = CamReader();
+read.wait_event_download();
