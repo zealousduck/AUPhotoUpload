719e887c311fb9b28e6057e60e085bcfba81b128
zealousduck <stewartpatrick12@gmail.com>
2016-03-01 11:23:39 -0600

    OSX-specific bugs; need to be ironed out on a Linux box

--- production/Reader.py
+++ production/Reader.py
@@ -10,6 +10,7 @@ import time
 import os
 import thread
 import subprocess
+from Queue import Queue
 
 class Reader(object):
 
@@ -23,18 +24,17 @@ class Reader(object):
         self.myOrders = orders
         # Extract relevant config data
         self.directoryName = config.get('directories', 'imagedirectory')
-        os.chdir(self.directoryName)
+        #os.chdir(self.directoryName)
     
     def run(self):
         print "Hi, I'm a Reader!"
-        threadStarted = False
+        threadId = -1
         while not self.myOrders.empty():
             time.sleep(constants.POLL_TIME)
-            if not threadStarted:
-                thread.start_new_thread(self.wait_event_download)
-                threadStarted = True
+            if (threadId == -1):
+                thread.start_new_thread(self.wait_event_download, ())
+                threadId = 555
             print "Reader, checking in! pid:", os.getpid()
-        
         print "Reader is exiting."
         self.toggle_off()
         # Put actual cleanup/saving code here!
@@ -43,16 +43,20 @@ class Reader(object):
     def wait_event_download(self):
         try:
             # Adds pictures to the stored photo directory. If no camera is supported, we raise an error.
-            subprocess.check_output(["gphoto2", "--wait-event-and-download", "--skip-existing", self.dirBrowser], self.shell);
+            subprocess.check_output(["gphoto2", "--wait-event-and-download", "--skip-existing", self.dirBrowser], self.shell)
         except subprocess.CalledProcessError:
             print "Camera is either not connected or not supported"
         
     def toggle_off(self):
         try:
-            subprocess.check_output(["killall", "gphoto2"], self.shell);
+            subprocess.check_output(["killall", "gphoto2"], self.shell)
         except subprocess.CalledProcessError:
             print "Gphoto2 not a running proccess."    
         finally:
             exit  
-
-
+            
+# 
+# q = Queue()
+# q.put("item")
+# rdr = Reader(q)
+# rdr.wait_event_download()
