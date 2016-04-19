3938ea95ab85b529dc12b445169eccc5d7576604
zealousduck <stewartpatrick12@gmail.com>
2016-04-19 17:17:09 -0500

    Debug statements to hunt down the disappearance of HANDLE_NONE

--- production/Handler.py
+++ production/Handler.py
@@ -64,6 +64,7 @@ class Handler(object):
             renamedList = []
             for element in listToUpload:
                 renamedList.append(self.renameWithTimestamp(element))
+            print 'len(listToUpload', len(listToUpload)
             if len(listToUpload) != 0:
                 self.exitMessage = Utility.QMSG_UPLOAD_DONE
                 f = open(self.listFileName, 'a')
@@ -88,6 +89,7 @@ class Handler(object):
                     except OSError:
                         print "failed to delete file " + element + "."
             else:
+                print 'QMSG_HANDLE_NONE'
                 self.exitMessage = Utility.QMSG_HANDLE_NONE       
         # once uploader done, exit/send a message to supervisor "done" and then exit
         self.orders.put(self.exitMessage)
