175e6c8f05feeb44f8e2c5ad243b0f54adf84786
zealousduck <stewartpatrick12@gmail.com>
2016-04-19 16:32:53 -0500

    Commented functionality of various Supervisor functions; various clean-ups; extraction of QMSG_START functionality into a startUploadJob() function; separated didScanFail conditionout of startUploadJob() and into its own condition on QMSG_START (should probably have a QMSG_START and a QMSG_TRY_SCAN later?)

--- production/Supervisor.py
+++ production/Supervisor.py
@@ -18,7 +18,7 @@ from multiprocessing import Process, Queue
 class Supervisor(object):
 
     def __init__(self):
-        self.guiQueue = Queue()
+        self.userInputQueue = Queue()
         self.handlerQueue = Queue()
         self.readerQueue = Queue()
         self.statusQueue = Queue()
@@ -29,33 +29,64 @@ class Supervisor(object):
         self.handlerDelayed = False
         self.stableInternetCounter = 0
     
+    '''
+    startGUI() initializes and runs the GUI. It is started once and only
+        terminated at the end of the program execution. Messages can be passed
+        to the GUI via self.statusQueue so that the Supervisor can update the GUI
+        based on background work. Messages are passed back to the Supervisor via
+        self.userInputQueue, based on GUI button presses. 
+        startGUI() is intended to be hooked into a multiprocessing Process() call.
+    '''    
+    def startGUI(self):
+        myGui = tsgui.FrontEnd(self.userInputQueue, self.statusQueue)
+        myGui.run()
+    
+    '''
+    startReader() initializes and runs the Reader. It is started at the beginning
+        of every workflow, when the User presses "Start Upload". Messages can be
+        passed to and from the Reader via self.readerQueue.
+    '''
     def startReader(self):
         reader = Reader.Reader(self.readerQueue)
         reader.run()
         
+    '''
+    runReader() starts the Reader process and manages the QMSG orders placed in
+        both the Reader queue and the GUI status queue.
+    '''
     def runReader(self):
         self.readerProcess = Process(target = self.startReader)
         self.readerQueue.put(Utility.QMSG_SCAN)
         self.statusQueue.put(Utility.QMSG_SCAN)
         self.readerProcess.start()
-        # wait for reader to finish scanning
         self.readerProcess.join()
         
+    '''
+    startHandler() initializes and runs the Handler. It is started after the Reader
+        in the workflow. Messages can be passed to and from the Handler via 
+        self.handlerQueue.
+    '''
     def startHandler(self):
         handler = Handler.Handler(self.handlerQueue)
         handler.run()
-        
+            
+    '''
+    runHandler() starts the Handler process and manages the QMSG orders placed
+        in the Handler's queue. It additionally marks that Handler processing has
+        begun.
+    '''
     def runHandler(self):
         self.handlerProcess = Process(target = self.startHandler)
         self.handlerQueue.put(Utility.QMSG_HANDLE)
         self.handlerProcess.start()
         self.handlerDelayed = False
         
-    def startGUI(self):
-        myGui = tsgui.FrontEnd(self.guiQueue, self.statusQueue)
-        myGui.run()
-        
-    def createImageDir(self):
+    '''
+    verifyImageDirectory() makes sure that the program can access the necessary
+        directory for the images it is handling. If it does not exist, it will
+        create it.
+    '''
+    def verifyImageDirectory(self):
         config = Utility.getProjectConfig()
         imgdir = config.get('directories','imagedirectory')
         print imgdir
@@ -64,10 +95,13 @@ class Supervisor(object):
                 os.makedirs(imgdir)
             except OSError as exception:
                 if exception.errno != errno.EEXIST:
-                    raise
-        
-        
-    def checkInternet(self):
+                    raise Exception('failed to create image directory')     
+    
+    '''
+    initializeInternet() establishes whether the program has internet connectivity
+        at program start-up. The GUI will be updated accordingly.
+    '''
+    def initializeInternet(self):
         self.stableInternetCounter = 0
         for i in range(Utility.STABLE_INTERNET_COUNT):
             if Utility.checkInternetConnection():
@@ -78,24 +112,38 @@ class Supervisor(object):
         else:
             self.statusQueue.put(Utility.QMSG_INTERNET_NO)
             self.stableInternet = False
-            
+    
+    '''
+    updateInternet() periodically checks whether the internet is stable enough
+        to be used. self.stableInternetCounter acknowledges the potential
+        for time-outs and instead allows some rubber-banding of connectivity.
+    '''
     def updateInternet(self):
         if Utility.checkInternetConnection():
-            if self.stableInternetCounter < Utility.STABLE_INTERNET_COUNT:
+            if (self.stableInternetCounter < Utility.STABLE_INTERNET_COUNT):
                 self.stableInternetCounter += 1
-            else:
+            if (self.stableInternetCounter >= Utility.STABLE_INTERNET_COUNT):
                 self.stableInternet = True
                 self.statusQueue.put(Utility.QMSG_INTERNET_YES)
                 #print 'DEBUG: checkInternetConnection() == True'
         else:
             if (self.stableInternetCounter > 0): # only count down to 0
                 self.stableInternetCounter -= 1 
-            if self.stableInternetCounter < Utility.STABLE_INTERNET_COUNT/2:
+            if (self.stableInternetCounter < Utility.STABLE_INTERNET_COUNT/2):
                 self.stableInternet = False
                 self.statusQueue.put(Utility.QMSG_INTERNET_NO)
                 #print 'DEBUG: checkInternetConnection() == False'
-        print 'DEBUG: stableInternet:', self.stableInternet, 'stableInternetCounter:', self.stableInternetCounter
-        
+        #print 'DEBUG: stableInternet:', self.stableInternet, 'stableInternetCounter:', self.stableInternetCounter
+    
+    '''
+    tryScan() implements an alternative workflow for Supervisor that does not
+        pull any images from the camera or upload anything. Instead, it enables
+        Supervisor to establish a "baseline" for images to compare to. tryScan()
+        is intended for when camera connectivity is faulty, resulting in 
+        corrupt or missing baselines.
+        A baseline for images is necessary, otherwise every image on the camera
+        will be uploaded!
+    '''
     def tryScan(self):
         try:
             self.statusQueue.put(Utility.QMSG_SCAN)
@@ -106,6 +154,12 @@ class Supervisor(object):
             self.statusQueue.put(Utility.QMSG_SCAN_FAIL)
             self.didScanFail = True
             
+    '''
+    isScanMessageFail() is a boolean function that checks whether a camera scan
+        is faulty (in conjunction with tryScan()). The results of the scan are
+        pushed to the GUI for display. This function returns True if the scan
+        fails, and False otherwise.
+    '''
     def isScanMessageFail(self):
         messageStatus = False
         scanMsg = Utility.readMessageQueue(self.readerQueue)
@@ -116,8 +170,15 @@ class Supervisor(object):
             self.statusQueue.put(Utility.QMSG_SCAN_DONE)
         else:
             print "Something went wrong with the ReaderMsgQueue!"
+            messageStatus = False
         return messageStatus
     
+    '''
+    processHandlerMsg() retrieves messages from the Handler queue since 
+        Handler(Uploader) processing occurs in the background, with the chance
+        to regularly send the Supervisor status updates. This function retrieves
+        and passes those messags onto the GUI.
+    '''
     def processHandlerMsg(self):
         if not self.handlerQueue.empty():
             handlerMsg = Utility.readMessageQueue(self.handlerQueue) 
@@ -130,48 +191,45 @@ class Supervisor(object):
             else:
                 self.statusQueue.put("Unknown Message from handlerQueue")
     
+    def startUploadJob(self):
+        self.runReader()
+        if self.isScanMessageFail():
+            continue
+        if self.stableInternet: # only start Handler if stable connection
+            self.runHandler()
+        else:
+            time.sleep(Utility.POLL_TIME)
+            self.handlerDelayed = True
+    
+    '''
+    run() is the entry-point and main loop for the PhotoUpload program.
+        run() is where the Supervisor coordinates the various children and workflow
+        activities.
+        Write something more descriptive here later?
+    '''
     def run(self):
-        print "Supervisor, checking in! pid:", os.getpid()
-        # If image directory does not exist yet, create it!
-        self.createImageDir()
-        guiProcess = Process(target = self.startGUI)
-        guiProcess.start()
-        # Establish whether we have stable internet
-        self.checkInternet()
-        # Initialize with all images currently on camera
+        self.verifyImageDirectory()
+        Process(target = self.startGUI).start()
+        self.initializeInternet()
         self.tryScan()
         time.sleep(Utility.POLL_TIME)
         while True:
-            if not self.guiQueue.empty():
-                job = self.guiQueue.get()
-                if job == Utility.QMSG_START:
+            if not self.userInputQueue.empty():
+                job = self.userInputQueue.get()
+                if (job == Utility.QMSG_START and self.didScanFail):
+                    print "Supervisor handles Upload job here"
                     if self.didScanFail:
                         self.tryScan()
-                        continue # cannot complete job as normal if no baseline scan
-                        # REFACTOR THIS CODE, DON'T FORGET! try Supervisor.initialScan()
-                    print "Supervisor handles Upload job here"
-                    self.runReader()
-                    if self.isScanMessageFail():
-                        continue
-                    if self.stableInternet: # only start Handler if stable connection
-                        self.runHandler()
-                    else:
-                        time.sleep(Utility.POLL_TIME)
-                        self.handlerDelayed = True
-                elif job == Utility.QMSG_SETTINGS:
+                elif (job == Utility.QMSG_START and not self.didScanFail):
+                    self.startUploadJob()
+                elif (job == Utility.QMSG_SETTINGS):
                     print "Supervisor handles Settings job here if needed"
                 else:
                     raise Exception('Supervisor.run:  unexpected object in queue')
-            # endif self.guiQueue.empty()
-            
-            # Start upload if delayed and internet is now stable
+            # endif self.userInputQueue.empty()
             if self.handlerDelayed and self.stableInternet:
                 self.runHandler()
-            
-            time.sleep(Utility.POLL_TIME) # wait for handlerProcess to actually start
             self.processHandlerMsg()
-            
-            # Check current internet connection, allowing for some fluctuation in results    
             self.updateInternet()
             time.sleep(Utility.POLL_TIME)
         # end while loop
