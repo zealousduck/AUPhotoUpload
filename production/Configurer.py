bc86ace1625b5848b6479114edaaf17f6f18ff97
emseduau <ems0025@auburn.edu>
2016-04-28 05:16:32 -0500

    Update Configurer.py

--- production/Configurer.py
+++ production/Configurer.py
@@ -28,10 +28,10 @@ class Configurer(object):
             self.config.add_section('directories')
             self.config.add_section('dropboxinfo')
             self.config.add_section('carddata')
-            self.config.set('directories', 'imagedirectory', '_photo_upload_img_directory')   # current
-            self.config.set('dropboxinfo', 'key', 'kpwogxeclcczgmf')
-            self.config.set('dropboxinfo', 'secret', 'tq1fl93eraqbh97')
-            self.config.set('dropboxinfo', 'accesstoken', 'n0d7CWbJI0AAAAAAAAAACHuj83rJmyPJsFveoeZore8O7xctu8NfaC0EwnEiWB7I')
+            self.config.set('directories', 'imagedirectory', '')   # current
+            self.config.set('dropboxinfo', 'key', '')
+            self.config.set('dropboxinfo', 'secret', '')
+            self.config.set('dropboxinfo', 'accesstoken', '')
             self.config.set('carddata', 'recentcardid', 00000000)
             self.saveSettings('photoUploadDefaults.cfg')
         # copyfile completely replaces the old file
